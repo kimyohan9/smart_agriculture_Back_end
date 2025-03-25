7f09b51d912af2d52df3ea7b8c5eb037d3841fe0
shincc6500 <130081283+shincc6500@users.noreply.github.com>
2025-03-25 17:43:13 +0900

    update : chatbot앱에 model 영역 추가.
    model에 테이블을 추가하여 address정보 및 recommendation 정보를 저장할 수 있도록 함.
    이를 통해서 PNU code, 좌표 등의 정보 및 추천 내역이 DB에 저장됨.

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,6 +2,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from .utils import address_info,soilexam,SoilExamRAG
+from .models import CropRecommendation,Adressinformation
 
 def get_address_info_view(request):
     address = request.GET.get('address', '전라남도 해남군 산이면 덕송리 751')  # 디폴트 값 설정
@@ -13,11 +14,24 @@ def get_address_info_view(request):
     add_info = address_info(type, address)
     if not add_info:
         return JsonResponse({"error": "유효하지 않은 주소입니다."}, status=400)
+    if add_info:
+        Adressinformation.objects.create(
+            pnu_code=add_info['id'],
+            address=add_info['address'],
+            point=add_info['point']
+        )
     
     return JsonResponse({"address_information": add_info})
 
 def get_soil_data(request):
-    PNU_Code = request.GET.get('address_information[id]', '4682041029107510000')
+    address = request.GET.get('address', '전라남도 해남군 산이면 덕송리 751')  # 디폴트 값 설정
+    type = request.GET.get('type', 'PARCEL')  # 디폴트 값 설정
+    
+    if not address:
+        return JsonResponse({"error": "주소를 입력해 주세요."}, status=400)
+    
+    add_info = address_info(type, address)
+    PNU_Code = add_info['id']
     if not PNU_Code:
         return JsonResponse({"error": "PNU_Code를 찾을 수 없습니다."}, status=400)
     
@@ -29,13 +43,27 @@ def get_soil_data(request):
     return JsonResponse({"soil_data": soil_data})
 
 def crop_recommendation_view(request):
-    PNU_Code = request.GET.get('address_information[id]', '4682041029107510000')
+    address = request.GET.get('address', '전라남도 해남군 산이면 덕송리 751')  # 디폴트 값 설정
+    type = request.GET.get('type', 'PARCEL')  # 디폴트 값 설정
+    
+    if not address:
+        return JsonResponse({"error": "주소를 입력해 주세요."}, status=400)
+    
+    add_info = address_info(type, address)
+    PNU_Code = add_info['id']
     if not PNU_Code:
         return JsonResponse({"error": "PNU_Code를 찾을 수 없습니다."}, status=400)
     
     rag_system = SoilExamRAG(PNU_Code=PNU_Code)
-    recommendation = rag_system.get_recommendation()
-    if not recommendation:
+    recommendations = rag_system.get_recommendation()
+    if not recommendations:
         return JsonResponse({"message": "토지 정보를 얻지 못하였습니다."}, status=404)
     
-    return JsonResponse({"recommendations": recommendation})
+    for rec in recommendations:
+        CropRecommendation.objects.create(
+            crop=rec["crop"],
+            reason=rec["reason"],
+            crop_info=rec["crop_info"]
+        )
+    
+    return JsonResponse({"recommendations": recommendations})

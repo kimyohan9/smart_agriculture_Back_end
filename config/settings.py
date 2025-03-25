21a9435b2dcce1270f1833a468f883228cd143b6
jhmang1128 <jhmang1128@gmail.com>
2025-03-25 19:15:49 +0900

    💡 Update comments of settings

--- config/settings.py
+++ config/settings.py
@@ -11,31 +11,52 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 import os
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+#### SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-x0g7c$n$l+5#!pcp_6bl@$8lvqznwp!r31oq(yf8q8f!r!8d6p"
+# SECRET_KEY = os.getenv('api_key') # 예시
+
+
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-# SECURITY WARNING: don't run with debug turned on in production!
+
+
+#### SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 # LOGOUT_REDIRECT_URL = "/"
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",  # 프론트엔드 URL 추가
-]
-# Application definition
+
+#### acess permmision (외부 접속 허용 list)
+CORS_ALLOWED_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:5500", # live server
+#     "https://aicropmate.com", # 구매한 domain
+# ]
+
+
+#### Application definition
 AUTH_USER_MODEL = 'users.CustomUser'
+
+
+#############################################################################################
+### INSTALLED_APPS
+#############################################################################################
 INSTALLED_APPS = [
+    #### default
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -43,22 +64,36 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sites",
+    
+    #### drf
     "rest_framework",
     "rest_framework.authtoken",
+    
+    
+    #### 외부 통신?
+    'corsheaders', 
 
+    
+    #### auth
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.kakao", #카카오톡
-    'corsheaders',
+    
+    
+    #### app
     "chatbot",
-    "post",    
+    "post",
     "crawled_data",
-    "users",  
+    "users",
 ]
 
+
 MIDDLEWARE = [
+    #### cors
     'corsheaders.middleware.CorsMiddleware',
+    
+    #### default
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -66,15 +101,20 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
+    #### allauth
     "allauth.account.middleware.AccountMiddleware",
+    
 ]
 
+
 ROOT_URLCONF = "config.urls"
+
 
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates'],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -87,11 +127,14 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = "config.wsgi.application"
 
 
+#############################################################################################
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+#############################################################################################
 
 DATABASES = {
     "default": {
@@ -100,7 +143,12 @@ DATABASES = {
     }
 }
 
-SOCIALACCOUNT_PROVIDERS = {
+
+#############################################################################################
+# kakao
+#############################################################################################
+
+    SOCIALACCOUNT_PROVIDERS = {
     "kakao": {
         "APP": {
             "client_id": "a6971a25bb35dc1113d81b5713a3ccc7",  # ✅ 여기에 카카오 REST API 키 입력
@@ -111,8 +159,10 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 
+#############################################################################################
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+#############################################################################################
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -130,20 +180,25 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+#############################################################################################
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
+#############################################################################################
 
+#### lang
 LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
 USE_I18N = True
 
+#### time zone
+TIME_ZONE = "UTC"
 USE_TZ = True
 
 
+
+#############################################################################################
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+#############################################################################################
 
 STATIC_URL = "/static/"
 
@@ -154,8 +209,12 @@ STATICFILES_DIRS = [
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+
+
+#############################################################################################
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+#############################################################################################
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
@@ -164,8 +223,10 @@ AUTHENTICATION_BACKENDS = [
     "allauth.account.auth_backends.AuthenticationBackend",
 ] #카카오톡
 
+
 SITE_ID = 1  # Django 사이트 ID
 # LOGIN_REDIRECT_URL = "users"  # 로그인 후 이동할 페이지
+
 
 SOCIALACCOUNT_LOGIN_ON_GET = True
 # ACCOUNT_LOGOUT_REDIRECT_URL = "users"  # 로그아웃 후 이동할 페이지
